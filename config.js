73973e0f7add4744715cf5363e3efad3d9e9759c
黎胖砸 <2875997232@qq.com>
2022-08-28 01:38:31 +0800

    2

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '熊熊的站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1412172-883ad5bdc77cc69b57c94e6f',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://lipangza.github.io/'
+    },
+    {
+      text: '博客',
+      url: 'https://lipangza.github.io/blog'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/lipangza/'
+    },
+  ],
+};
